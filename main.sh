c45fa7ee0a2d176fc3959d99f20c7d97abd67183
smazmi <sadiquemobaraka5@gmail.com>
2024-04-04 19:16:09 +0530

    Fix: Issue with fish shell and changed .env to .profile

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check if you are connected to a LPU wifi
 check_lpu_wifi() {
     if [ "$(nmcli -t -f active,ssid dev wifi | grep -E '^yes' | grep -c LPU)" == "1" ]; then
         return 0 
@@ -8,20 +9,22 @@ check_lpu_wifi() {
     fi
 }
 
+# Reading and stroing the LPU credentials depending on the shell you are using
 store_lpu_credentials() {
     read -p "Enter your LPU username: " username
     read -sp "Enter your LPU password: " password
     echo
 
-    echo "LPU_USERNAME=\"$username\"" > ~/.env
-    echo "LPU_PASSWORD=\"$password\"" >> ~/.env
+    echo "LPU_USERNAME=\"$username\"" > ~/.profile
+    echo "LPU_PASSWORD=\"$password\"" >> ~/.profile
 
     if [ "$SHELL" = "/bin/bash" ] || [ "$SHELL" = "/usr/bin/bash" ] || [ "$SHELL" = "bash" ]; then
-        echo "source ~/.env" >> ~/.bashrc
+        echo "source ~/.profile" >> ~/.bashrc
     elif [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ] || [ "$SHELL" = "zsh" ]; then
-        echo "source ~/.env" >> ~/.zshrc
+        echo "source ~/.profile" >> ~/.zshrc
     elif [ "$SHELL" = "/usr/bin/fish" ] || [ "$SHELL" = "/usr/bin/fish" ] || [ "$SHELL" = "fish" ]; then
-        echo "source ~/.env" >> ~/.config/fish/config.fish
+      echo "set -gx LPU_USERNAME \"$username\"" >> ~/.config/fish/config.fish
+      echo "set -gx LPU_PASSWORD \"$password\"" >> ~/.config/fish/config.fish   # who tf uses fish
     else
         echo "Unsupported shell. Please manually set the environment variables."
     fi
@@ -29,6 +32,7 @@ store_lpu_credentials() {
     echo "LPU username and password have been stored securely."
 }
 
+# Login to LPU wifi
 perform_lpu_login() {
     username="$LPU_USERNAME"
     password="$LPU_PASSWORD"
@@ -36,7 +40,7 @@ perform_lpu_login() {
     data="mode=191&username=$username%40lpu.com&password=$password"
     res=$(curl -s 'https://10.10.0.1/24online/servlet/E24onlineHTTPClient' --data-raw $data --compressed --insecure)
 
-    if [[ $res == *"To start surfing"* ]]; then
+    if [[ $res == *"To start surfing"* ]]; then # lmao
         echo "Login successful"
         notify-send "LPU Login" "Login successful" -i network-wireless
     else
@@ -60,8 +64,9 @@ main() {
     fi
 }
 
-main
+main  # Calling the main function
 
+# Sourcing the shell configuration files
 if [ "$SHELL" = "/bin/bash" ] || [ "$SHELL" = "/usr/bin/bash" ] || [ "$SHELL" = "bash" ]; then
     source ~/.bashrc
 elif [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ] || [ "$SHELL" = "zsh" ]; then
@@ -70,4 +75,4 @@ elif [ "$SHELL" = "/usr/bin/fish" ] || [ "$SHELL" = "/usr/bin/fish" ] || [ "$SHE
     source ~/.config/fish/config.fish
 fi
 
-perform_lpu_login
+perform_lpu_login # Calling the login function
